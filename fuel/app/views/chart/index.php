61820ed16ca5a4afbbec0ce3689e14fff4765987
y1uNo <j124039@g.kogakuin.jp>
2025-08-22 21:41:34 +0900

    ボタンの位置を変更

--- fuel/app/views/chart/index.php
+++ fuel/app/views/chart/index.php
@@ -117,6 +117,11 @@
                         
                         <!-- フローチャート操作ボタン -->
                         <div class="chart-controls">
+                            <div class="control-button" title="Mermaidコード表示" data-bind="click: $root.showMermaidCode">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                                </svg>
+                            </div>
                             <div class="control-button" title="元に戻す (Ctrl+Z)" data-bind="click: $root.undo, css: { disabled: !canUndo() }">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
@@ -145,11 +150,6 @@
                             <div class="control-button" title="ズームリセット" data-bind="click: $root.resetZoomAndPan">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
-                                </svg>
-                            </div>
-                            <div class="control-button" title="Mermaidコード表示" data-bind="click: $root.showMermaidCode">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
                                 </svg>
                             </div>
                             <div class="control-button" title="ログアウト" data-bind="click: $root.logout">
