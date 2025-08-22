928970f18d51ba682b9cd264578e2adf189ac00f
y1uNo <j124039@g.kogakuin.jp>
2025-08-22 21:09:47 +0900

    初期ノードを変更

--- public/assets/js/chart/config.js
+++ public/assets/js/chart/config.js
@@ -13,3 +13,6 @@ const API_ENDPOINTS = {
     logout: `${API_BASE}/logout`,
     authStatus: `${API_BASE}/status`
 };
+
+// デフォルトチャート設定
+const DEFAULT_CHART_CONTENT = 'graph LR\n    node_start[開始]\n    node_process[処理]\n    node_end[終了]\n    node_start --> node_process\n    node_process --> node_end';
