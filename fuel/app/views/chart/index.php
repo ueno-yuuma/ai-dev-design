9b024ba30db0ed825f1b8c0d06c769277857cfb3
y1uNo <j124039@g.kogakuin.jp>
2025-08-22 21:30:22 +0900

    mermaidコードを確認する機能を追加

--- fuel/app/views/chart/index.php
+++ fuel/app/views/chart/index.php
@@ -147,6 +147,11 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
                                 </svg>
                             </div>
+                            <div class="control-button" title="Mermaidコード表示" data-bind="click: $root.showMermaidCode">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                                </svg>
+                            </div>
                             <div class="control-button" title="ログアウト" data-bind="click: $root.logout">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
